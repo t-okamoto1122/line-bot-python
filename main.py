49d97e885a0496060460418f78556e3ab7a6e63c
“t-okamoto1122“ <taka.okamoto1122@gmail.com>
2018-02-17 20:51:16 -0500

    edit message_template

--- main.py
+++ main.py
@@ -80,12 +80,10 @@ def message_text(event):
     template_message = TemplateSendMessage(
         alt_text='Buttons template',
         template=ButtonsTemplate(
-            thumbnail_image_url='https://example.com/image.jpg',
-            title='Menu', text='Please select',
+            title='問題No.', text=question,
             actions=[
                 PostbackTemplateAction(
-                    label='postback', text='postback text',
-                    data='action=buy&itemid=1'
+                    label='answer', text=answer
                 )
             ]
         )
