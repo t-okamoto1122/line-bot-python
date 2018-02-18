aaf43466bed4ba84ce7089624bc916587269ceb1
“t-okamoto1122“ <taka.okamoto1122@gmail.com>
2018-02-17 19:54:34 -0500

    create text from spreadsheet

--- reply_msg.py
+++ reply_msg.py
@@ -1,3 +1,32 @@
+
+import os
+import sys
+import gspread
+import oauth2client; print(oauth2client.__version__)
+from oauth2client.service_account import ServiceAccountCredentials
+from numpy.random import *
+
+
+# 最終行を取得
+def create_random_num(worksheet):
+    str_list = list(filter(None, sheet.col_values(1)))  # fastest
+    return random_integers(len(str_list))
+
+
+scope = ['https://spreadsheets.google.com/feeds']
+
+# 3でダウンロードしたjsonファイルを指定する
+credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(credentials)
+
+sheet = client.open("English-line-bot").sheet1
+random = create_random_num(sheet)
+print(random)
+question = sheet.cell(int(random), 3).value
+
+print(question)
+
+
 class Reply:
     # def __init__(self, reply_msg , answer):
     #     self.reply_msg = reply_msg
@@ -7,11 +36,5 @@ class Reply:
         print("Reply init")
 
     def reply(self, request_text):
-        if request_text == "a":
-            return "No.1"
-        elif request_text == "b":
-            return "No.2"
-        else:
-            return "No.3"
-
+        return question
 
